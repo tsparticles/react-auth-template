bcd9ac737c02844a9329f2aa8d712850d332b42f
Matteo Bruni <ar3s@icloud.com>
2020-07-29 09:01:10 +0200

    Updates

---
+++ precache-manifest.f42e9cccfad3bbfc71a19cdeaccf7b41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f030b4a510c5c69c1383b6f3894160",
+    "url": "/react-auth-template/index.html"
+  },
+  {
+    "revision": "784b9fbc48d2766e7f7c",
+    "url": "/react-auth-template/static/css/main.db7423eb.chunk.css"
+  },
+  {
+    "revision": "bc3c5feb4c040a94dec3",
+    "url": "/react-auth-template/static/js/2.badf48fa.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/react-auth-template/static/js/2.badf48fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784b9fbc48d2766e7f7c",
+    "url": "/react-auth-template/static/js/main.cd843539.chunk.js"
+  },
+  {
+    "revision": "bcd97a008b59f5a61d67",
+    "url": "/react-auth-template/static/js/runtime-main.a775e72e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-auth-template/static/media/logo.5d5d9eef.svg"
+  }
+]);
